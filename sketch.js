0cec3269635c5274a120afa85db6c4a5cb229842
Arth127 <122410619+Arth127@users.noreply.github.com>
2023-02-09 20:43:51 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+function preload(){
+seaImg = loadImage ("sea.png");
+
+shipImg1 = loadAnimation ()
+
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+    drawSprites();
+
+ 
+}
