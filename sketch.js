6dbe3f2c11960e5fade29c402baf3db96eed07cb
Migbu <122410343+Migzeca@users.noreply.github.com>
2023-02-15 18:46:04 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,33 @@
+var navioImg,navio,mar,marImg
+
 
 function preload(){
-seaImg = loadImage ("sea.png");
+navioImg = loadAnimation  ("ship-1.png" , "ship-2.png" ,"ship-3.png" ,"ship-4.png");
 
-shipImg1 = loadAnimation ()
-
+marImg = loadImage ("sea.png")
 
 }
 
 function setup(){
-  createCanvas(400,400);
+  createCanvas(1400,1000);
   
+mar = createSprite(500,500,600,20)
+mar.addImage(marImg);
+mar.velocityX = -2;
+
+
+if (mar.x < 0){
+ mar,x =mar.width/2;
+}
+
+navio = createSprite(700,1060,20,50);
+navio.addAnimation("navio" , navioImg);
+
+
 }
 
 function draw() {
-  background("blue");
+  background("white");
     drawSprites();
 
  
